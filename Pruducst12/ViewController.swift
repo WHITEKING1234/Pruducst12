8a195132310b9aa836e21f2fda625e451d17735b
bombag456 <bombag456@gmail.com>
2023-02-06 00:32:37 +0600

    Initial Commit

---
+++ Pruducst12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pruducst12
+//
+//  Created by Mac on 6/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
